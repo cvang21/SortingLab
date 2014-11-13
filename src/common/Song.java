263be491763df29766f10982540443ffbb42782b
cvadmin <cvadmin@10.30.20.48>
2014-11-12 18:56:10 -0600

    Added Contents to Song class and lab1

--- src/common/Song.java
+++ src/common/Song.java
@@ -1,5 +1,71 @@
 package common;
 
+import java.util.Objects;
+
 public class Song {
     
+    private String songTitle;
+    private String artist;
+    private String album;
+    private int trackNumber;
+
+    public String getSongTitle() {
+        return songTitle;
+    }
+
+    public void setSongTitle(String songTitle) {
+        this.songTitle = songTitle;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public int getTrackNumber() {
+        return trackNumber;
+    }
+
+    public void setTrackNumber(int trackNumber) {
+        this.trackNumber = trackNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + this.trackNumber;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Song other = (Song) obj;
+        if (this.trackNumber != other.trackNumber) {
+            return false;
+        }
+        return true;
+    }
+  
+    @Override
+    public String toString() {
+        return "Song{" + "songTitle=" + songTitle + ", artist=" + artist + ",  album=" + album + ", trackNumber=" + trackNumber + "'}'";
+    }
+            
 }
---
+++ src/lab1/Exercise1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+import common.Song;
+import org.apache.commons.lang3.builder.CompareToBuilder;
+
+/**
+ *
+ * @author cvadmin
+ */
+public class Exercise1 {
+    
+}
